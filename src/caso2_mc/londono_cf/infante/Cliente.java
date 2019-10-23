5051a77eb709f0acfa7fce866402acaa18c754a1
Mc Londono <mc.londono@uniandes.edu.co>
2019-10-23 18:51:35 -0500

    clase cliente

--- src/caso2_mc/londono_cf/infante/Cliente.java
+++ src/caso2_mc/londono_cf/infante/Cliente.java
@@ -1,7 +1,14 @@
 package caso2_mc.londono_cf.infante;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Random;
 
-public class Cliente {
+public class Cliente extends Thread{
+	public static final int PUERTO = 8000;
+
 	private static String AES = "AES";
 	private static String BLOWFISH ="BLOWFISH";
 	private static String RSA = "RSA"; 
@@ -10,8 +17,42 @@ public class Cliente {
 	private static String HMACSHA384 = "HMACSHA384";
 	private static String HMACSHA512 = "HMACSHA512";
 			
-	private int cedula;
+	private static String ERROR = "ERROR";
+	private static String OK = "OK";
 
 
+	
+	public static String getCedula()
+	{
+		Random rand = new Random();
+		return "" + rand.nextInt((5 - 1) + 1) + 1;
+	}
 
+	public static void main(String[] args) {
+		Random rand = new Random();
+		System.out.println(rand.nextInt((5 - 1) + 10000) * 4);
+	}
+	@Override
+	public void run() 
+	{
+		
+	}
+	
+	public void iniciar() {
+		try {
+			Socket socket = new Socket("157.253.227.92", PUERTO);
+			PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			BufferedReader lectorC = new BufferedReader(new InputStreamReader(System.in));
+			Protocolo.procedimiento(lectorC, bf, pw);
+			lectorC.close();
+			bf.close();
+			pw.close();
+			socket.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
 }
--- src/caso2_mc/londono_cf/infante/Servidor.java
+++ src/caso2_mc/londono_cf/infante/Servidor.java
@@ -1,5 +1,29 @@
 package caso2_mc.londono_cf.infante;
 
 public class Servidor {
+	private static String ERROR = "ERROR";
+	private static String OK = "OK";
+	
+	public void fail() 
+	{			
+		System.out.println(ERROR);
+	}
 
+	public void success() 
+	{
+		System.out.println(OK);
+	}
+
+	public void execute() 
+	{
+		Cliente client;
+		try 
+		{
+			client = new Cliente();
+			client.run();
+		} catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
 }
