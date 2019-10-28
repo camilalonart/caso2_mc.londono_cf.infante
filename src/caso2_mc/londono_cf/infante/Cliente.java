042fbe357f36106399cc132eca076d361856ba1e
Carlos Infante <cf.infante@uniandes.edu.co>
2019-10-27 20:38:53 -0500

    cambios menores al codigo

--- src/caso2_mc/londono_cf/infante/Cliente.java
+++ src/caso2_mc/londono_cf/infante/Cliente.java
@@ -9,97 +9,76 @@ import java.nio.charset.Charset;
 import java.util.Random;
 
 public class Cliente extends Thread{
-	
+
 	public static final int PUERTO = 8000;
 
 	// Authentication codes for algorithms
 	public static final String AES = "AES";
-	public static final String BLOWFISH ="BLOWFISH";
+	public static final String BLOWFISH ="Blowfish";
 	public static final String RSA = "RSA"; 
 	public static final String HMACSHA1 = "HMACSHA1"; 
 	public static final String HMACSHA256 = "HMACSHA256";
 	public static final String HMACSHA384 = "HMACSHA384";
 	public static final String HMACSHA512 = "HMACSHA512";
-	
+
 	public static final String ERROR = "ERROR";
 	public static final String OK = "OK";
-	
 
-	public static void main(String[] args) {
-		Cliente c = new Cliente();
-		c.start();
-	}
-	
+
 	public static String getClave() 
-    { 
+	{ 
 		Random rand = new Random();
 		int n = rand.nextInt((5 - 1) + 10) * 4;
-  
-        // chose a Character random from this String 
-        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                                    + "0123456789"
-                                    + "abcdefghijklmnopqrstuvxyz"; 
-  
-        // create StringBuffer size of AlphaNumericString 
-        StringBuilder sb = new StringBuilder(n); 
-  
-        for (int i = 0; i < n; i++) { 
-  
-            // generate a random number between 
-            // 0 to AlphaNumericString variable length 
-            int index 
-                = (int)(AlphaNumericString.length() 
-                        * Math.random()); 
-  
-            // add Character one by one in end of sb 
-            sb.append(AlphaNumericString 
-                          .charAt(index)); 
-        } 
-  
-        return sb.toString(); 
-    } 
-	
+
+		String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+				+ "0123456789"
+				+ "abcdefghijklmnopqrstuvxyz"; 
+
+		StringBuilder sb = new StringBuilder(n); 
+
+		for (int i = 0; i < n; i++)
+		{ 
+			int index = (int)(AlphaNumericString.length() * Math.random()); 
+
+			sb.append(AlphaNumericString.charAt(index)); 
+		} 
+
+		return sb.toString(); 
+	} 
+
 	public static String getCedula() 
-    { 
+	{ 
 		int n = 12;
-  
-        // chose a Character random from this String 
-        String AlphaNumericString = "0123456789";  
-        // create StringBuffer size of AlphaNumericString 
-        StringBuilder sb = new StringBuilder(n); 
-        
-        for (int i = 0; i < n; i++) { 
-  
-            // generate a random number between 
-            // 0 to AlphaNumericString variable length 
-            int index 
-                = (int)(AlphaNumericString.length() 
-                        * Math.random()); 
-  
-            // add Character one by one in end of sb 
-            sb.append(AlphaNumericString 
-                          .charAt(index)); 
-        } 
-  
-        return sb.toString(); 
-    } 
-	
+
+		String AlphaNumericString = "0123456789";  
+		StringBuilder sb = new StringBuilder(n); 
+
+		for (int i = 0; i < n; i++) { 
+
+			int index = (int)(AlphaNumericString.length() * Math.random()); 
+
+			sb.append(AlphaNumericString.charAt(index)); 
+		} 
+
+		return sb.toString(); 
+	} 
+
 	@Override
 	public void run() 
 	{
 		try 
 		{
 			iniciar();
-		} catch (Exception e) 
+		} 
+		catch(Exception e) 
 		{
 			System.err.println(e.getMessage());
 		}
 	}
-	
+
 	public void iniciar() {
 		try {
 
-//			Socket socket = new Socket("157.253.227.92", PUERTO);
 			Socket socket = new Socket("localhost", PUERTO);
 			PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
 			BufferedReader bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -113,7 +92,12 @@ public class Cliente extends Thread{
 		catch(Exception e){
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
+	public static void main(String[] args) {
+		Cliente c = new Cliente();
+		c.start();
+	}
+
 }
--- src/caso2_mc/londono_cf/infante/Protocolo.java
+++ src/caso2_mc/londono_cf/infante/Protocolo.java
@@ -5,29 +5,19 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.math.BigInteger;
-import java.security.InvalidKeyException;
 import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
 import java.security.PublicKey;
-import java.security.SecureRandom;
 import java.security.Security;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Date;
-import java.util.Random;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.Mac;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
@@ -40,256 +30,214 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
-import org.bouncycastle.util.encoders.Hex;
-
 
 public class Protocolo {
-	
+
 	public static void procedimiento(BufferedReader consoleReader, PrintWriter clientWriter, BufferedReader clientReader) {
-		
-//		java.security.cert.X509Certificate certificado = generarCertificado(llaves);
-//		byte[] certificadoEnBytes = certificado.getEncoded( );
-//		String certificadoEnString = printBase64Binary(certificadoEnBytes);
-//		socketParaComunicacion.println(certificadoEnString);
+
 		try {
-		String protocolLine;			
-		
-		clientWriter.println("HOLA");
-		protocolLine = clientReader.readLine();
-		if(protocolLine != null)
-		{
-			System.out.println(protocolLine);
-		}
-		if(protocolLine.equals("OK")) 
-		{
+			//ETAPA 1
 
-			String symmetricAlgorithm;
+			String protocolLine;			
 
-			int secretKeySize = 128;
-
-			int keyPairSize = 1024;
-			
-			//Se selecciona algoritmo simetrico
-
-			Random rand = new Random();
-			  int randomNum = rand.nextInt((2 - 1) + 1) + 1;
-			switch (randomNum) {
-//			case 1:
-//				symmetricAlgorithm = Cliente.AES;
-//				break;
-//			case 2:
-//				symmetricAlgorithm = Cliente.BLOWFISH;
-//				break;
-			default:
-				symmetricAlgorithm = Cliente.AES;
-				break;
-			}
-
-			String asymmetricAlgorithm = Cliente.RSA;
-
-
-			//seleccion de algoritmo HMAC
-//			System.out.println("Seleccione un algoritmo HMAC : ");
-//			System.out.println("1. HMACSHA1");
-//			System.out.println("2. HMACSHA256");
-//			System.out.println("3. HMACSHA384");
-//			System.out.println("4. HMACSHA512");
-//			linea = bfConsola.readLine();
-
-			
-			//seleccion de algoritmo HMAC
-			String authOption;
-			randomNum = rand.nextInt((4 - 1) + 1) + 1;
-
-			switch (randomNum) {
-			case 1:
-				authOption = Cliente.HMACSHA1;
-				break;
-			case 2:
-				authOption = Cliente.HMACSHA256;
-
-				break;
-			case 3:
-				authOption = Cliente.HMACSHA384;
-				break;
-			case 4:
-				authOption = Cliente.HMACSHA512;
-				break;
-			default :
-				authOption = Cliente.HMACSHA512;
-			}
-
-			String algorithms = "ALGORITMOS:"+symmetricAlgorithm+":"+asymmetricAlgorithm+":"+authOption;
-			
-			clientWriter.println(algorithms);
+			clientWriter.println("HOLA");
 			protocolLine = clientReader.readLine();
-					
-			if(protocolLine != null)
+
+			if(protocolLine.equals("OK")) 
 			{
-				System.out.println(protocolLine);
-			}
-				
-			//ETAPA 2
-			if(!protocolLine.equals(Cliente.ERROR))
-			{
-//				System.out.println(algorithms);
-				//Se genera el par de llaves: publica y privada
-				KeyPairGenerator kp = KeyPairGenerator.getInstance(Cliente.RSA);
-				kp.initialize(keyPairSize,new SecureRandom());
-				KeyPair keys = kp.generateKeyPair();
-				
-				//Creacion del certificado
-//				java.security.cert.X509Certificate certificado = generarCertificado(keys);
-//				byte[] certificadoEnBytes = certificado.getEncoded( );
-//				String certificadoEnString = printBase64Binary(certificadoEnBytes);
-//				clientWriter.println(certificadoEnString);
-				
-				String serverCertificate = "";
+
+				String symmetricAlgorithm;
+
+				int secretKeySize = 128;
+
+				//Se selecciona algoritmo simetrico
+				symmetricAlgorithm = Cliente.AES;
+
+				//			Random rand = new Random();
+				//			  int randomNum = rand.nextInt((2 - 1) + 1) + 1;
+				//			switch (randomNum) {
+				//			case 1:
+				//				symmetricAlgorithm = Cliente.AES;
+				//				break;
+				//			case 2:
+				//				symmetricAlgorithm = Cliente.BLOWFISH;
+				//				break;
+				//			default:
+				//				symmetricAlgorithm = Cliente.AES;
+				//				break;
+				//			}
+
+				String asymmetricAlgorithm = Cliente.RSA;
+
+				//seleccion de algoritmo HMAC
+				String macOption = Cliente.HMACSHA512;
+
+				//			randomNum = rand.nextInt((4 - 1) + 1) + 1;
+				//			switch (randomNum) {
+				//			case 1:
+				//				macOption = Cliente.HMACSHA1;
+				//				break;
+				//			case 2:
+				//				macOption = Cliente.HMACSHA256;
+				//
+				//				break;
+				//			case 3:
+				//				macOption = Cliente.HMACSHA384;
+				//				break;
+				//			case 4:
+				//				macOption = Cliente.HMACSHA512;
+				//				break;
+				//			default :
+				//				authOption = Cliente.HMACSHA512;
+				//			}
+
+				String algorithms = "ALGORITMOS:"+symmetricAlgorithm+":"+asymmetricAlgorithm+":"+macOption;
+
+				clientWriter.println(algorithms);
 				protocolLine = clientReader.readLine();
-				if(protocolLine != null)
+
+				//ETAPA 2
+				if(!protocolLine.equals(Cliente.ERROR))
 				{
-					serverCertificate = protocolLine;
-				}
 
-				//Obtener llave publica servidor
-				CertificateFactory cf = CertificateFactory.getInstance("X.509");
-				byte[] certificadoServerEnBytes = parserBase64Binary(serverCertificate);
-				InputStream in = new ByteArrayInputStream(certificadoServerEnBytes);
-				X509Certificate certificadoServer = (X509Certificate) cf.generateCertificate(in);	
-				
-				//Se obtiene llave publica del servidor
-				PublicKey llavePubServer = certificadoServer.getPublicKey();
-
-				//obtencion de llave secreta(simetrica)
-				KeyGenerator keygen = KeyGenerator.getInstance(symmetricAlgorithm);
-				keygen.init(secretKeySize);
-				SecretKey symmetricKey = keygen.generateKey();
-
-				//cifrado respecto a la publica del servidor
-				Cipher cifrador = Cipher.getInstance(asymmetricAlgorithm);
-				byte[] llaveSecretaEnBytes = symmetricKey.getEncoded();
-				cifrador.init(Cipher.ENCRYPT_MODE, llavePubServer);
-				byte[] byteCifradoLlaveSimetrica = cifrador.doFinal(llaveSecretaEnBytes);
-
-				//termina cifrado-------------------
-
-
-				//envio de llave simetrica cifrada con publica del servidor
-				clientWriter.println(printBase64Binary(byteCifradoLlaveSimetrica));
-				String reto = Cliente.getClave();
-				clientWriter.println(reto);
-
-				//Mensaje que envia el servidor cifrado con llave simetrica
-				protocolLine = clientReader.readLine();
-				
-				//Se desencripta el reto que envia de vuelta el servidor para ver si es la misma llave simetrica que generamos
-				cifrador = Cipher.getInstance(symmetricAlgorithm);
-				cifrador.init(Cipher.DECRYPT_MODE, symmetricKey);
-				byte[] descifrado = parserBase64Binary(protocolLine);
-				byte[] retoEnByte  = cifrador.doFinal(descifrado);
-				String retoServidor = printBase64Binary(retoEnByte);
-				
-				//verificamos igualdad entre el reto generado por cliente y el reto que envia el servidor
-				
-				boolean continuar = false;	
-				if(retoServidor.equals(reto))
-				{
-					System.out.println("Se realizo adecuadamente el intercambio de llave simetrica");
-					clientWriter.println("OK");
-					continuar = true;
-				}
-				else
-				{
-					System.out.println("No se intercambio correctamente la llave simetrica, lo sentimos.");
-					clientWriter.println("ERROR");
-				}
-
-				//Llave secreta(version del server)
-//				SecretKey key2 = new SecretKeySpec(retoEnByte, 0, retoEnByte.length, symmetricAlgorithm);
-				
-
-				if(continuar) 
-				{
-					//generamos datos
-					String datos = Cliente.getCedula();
-					byte[] datosEnBytes = parserBase64Binary(datos);
-					String clave = Cliente.getClave();
-					byte[] claveEnBytes = parserBase64Binary(clave);
-					
-					cifrador = Cipher.getInstance(symmetricAlgorithm);
-					cifrador.init(Cipher.ENCRYPT_MODE, symmetricKey);
-					byteCifradoLlaveSimetrica = cifrador.doFinal(datosEnBytes);
-					clientWriter.println(printBase64Binary(byteCifradoLlaveSimetrica));
-					
-					cifrador = Cipher.getInstance(symmetricAlgorithm);
-					cifrador.init(Cipher.ENCRYPT_MODE, symmetricKey);
-					byteCifradoLlaveSimetrica = cifrador.doFinal(claveEnBytes);
-					clientWriter.println(printBase64Binary(byteCifradoLlaveSimetrica));
-
-
-					//--------------------fin de envio de cifrado simetrico de los datos
-					
-					//Valor de monto ahorro que responde el servidor
+					String serverCertificate = "";
 					protocolLine = clientReader.readLine();
+					if(protocolLine != null)
+					{
+						serverCertificate = protocolLine;
+					}
+
+					//Obtener llave publica servidor
+					CertificateFactory cf = CertificateFactory.getInstance("X.509");
+					byte[] certificadoServerEnBytes = parserBase64Binary(serverCertificate);
+					InputStream in = new ByteArrayInputStream(certificadoServerEnBytes);
+					X509Certificate certificadoServer = (X509Certificate) cf.generateCertificate(in);	
+
+					//Se obtiene llave publica del servidor	
+					PublicKey llavePubServer = certificadoServer.getPublicKey();
+
+					//Se crea la llave simetrica
+					KeyGenerator keygen = KeyGenerator.getInstance(symmetricAlgorithm);
+					keygen.init(secretKeySize);
+					SecretKey symmetricKey = keygen.generateKey();
+
+					//Cifrado respecto a la llave publica del servidor
+					Cipher cifrador = Cipher.getInstance(asymmetricAlgorithm);
+					byte[] llaveSecretaEnBytes = symmetricKey.getEncoded();
+					cifrador.init(Cipher.ENCRYPT_MODE, llavePubServer);
+					byte[] byteCifradoLlaveSimetrica = cifrador.doFinal(llaveSecretaEnBytes);
+
+					//envio de llave simetrica cifrada con publica del servidor
+					clientWriter.println(printBase64Binary(byteCifradoLlaveSimetrica));
+					String reto = Cliente.getClave();
+					clientWriter.println(reto);
+
+					//Mensaje que envia el servidor cifrado con llave simetrica
+					protocolLine = clientReader.readLine();
+
+					//Se desencripta el reto que envia de vuelta el servidor para ver si es la misma llave simetrica que generamos
 					cifrador = Cipher.getInstance(symmetricAlgorithm);
 					cifrador.init(Cipher.DECRYPT_MODE, symmetricKey);
-					descifrado = parserBase64Binary(protocolLine);
-					byte[] montoEnByte  = cifrador.doFinal(descifrado);
-					String montoCliente = printBase64Binary(montoEnByte);
-					System.out.println("El monto es de: " + montoCliente);
-											
-					//Se descifra con la llave publica del servidor
-					protocolLine = clientReader.readLine();
-					cifrador = Cipher.getInstance(asymmetricAlgorithm);
-					cifrador.init(Cipher.DECRYPT_MODE, llavePubServer);
-					descifrado = parserBase64Binary(protocolLine);
-					byte[] hMacCifradoEnBytes  = cifrador.doFinal(descifrado);
-					String hMac = printBase64Binary(hMacCifradoEnBytes);
-					
-					//HMAC de los datos
-					Mac mac = Mac.getInstance(authOption);
-				    mac.init(symmetricKey);
-				    
-				    byte[] bytesHMacDecrypt = mac.doFinal(montoEnByte);
-					String hashCifradoenString = printBase64Binary(bytesHMacDecrypt);
+					byte[] descifrado = parserBase64Binary(protocolLine);
+					byte[] retoEnByte  = cifrador.doFinal(descifrado);
+					String retoServidor = printBase64Binary(retoEnByte);
 
-					if(hMac.equals(hashCifradoenString))
+					//verificamos igualdad entre el reto generado por cliente y el reto que envia el servidor
+
+					boolean continuar = false;	
+					if(retoServidor.equals(reto))
 					{
-						System.out.println("Se encontro que estaba acorde el valor con su hmac(valor)");
-						clientWriter.println(Cliente.OK);
+						clientWriter.println("OK");
+						continuar = true;
 					}
 					else
 					{
-						System.out.println("No es igual valor con hmac(valor)");
-						clientWriter.println(Cliente.ERROR);
+						System.out.println("Error. No se intercambio bien la llave simetrica");
+						clientWriter.println("ERROR");
+					}
+
+					if(continuar) 
+					{
+						//Generamos los datos
+						String datos = Cliente.getCedula();
+						byte[] datosEnBytes = parserBase64Binary(datos);
+						String clave = Cliente.getClave();
+						byte[] claveEnBytes = parserBase64Binary(clave);
+
+						cifrador = Cipher.getInstance(symmetricAlgorithm);
+						cifrador.init(Cipher.ENCRYPT_MODE, symmetricKey);
+						byteCifradoLlaveSimetrica = cifrador.doFinal(datosEnBytes);
+						clientWriter.println(printBase64Binary(byteCifradoLlaveSimetrica));
+						
+						cifrador = Cipher.getInstance(symmetricAlgorithm);
+						cifrador.init(Cipher.ENCRYPT_MODE, symmetricKey);
+						byteCifradoLlaveSimetrica = cifrador.doFinal(claveEnBytes);
+						clientWriter.println(printBase64Binary(byteCifradoLlaveSimetrica));
+						
+						//Valor de monto ahorro que responde el servidor
+						protocolLine = clientReader.readLine();
+						cifrador = Cipher.getInstance(symmetricAlgorithm);
+						cifrador.init(Cipher.DECRYPT_MODE, symmetricKey);
+						descifrado = parserBase64Binary(protocolLine);
+						byte[] montoEnByte  = cifrador.doFinal(descifrado);
+						String montoCliente = printBase64Binary(montoEnByte);
+						System.out.println("El monto es de " + montoCliente + " COP");
+						
+						//Se descifra con la llave publica del servidor
+						protocolLine = clientReader.readLine();
+						cifrador = Cipher.getInstance(asymmetricAlgorithm);
+						cifrador.init(Cipher.DECRYPT_MODE, llavePubServer);
+						descifrado = parserBase64Binary(protocolLine);
+						byte[] hMacCifradoEnBytes  = cifrador.doFinal(descifrado);
+						String hMac = printBase64Binary(hMacCifradoEnBytes);
+						
+						//HMAC de los datos
+//						long tiempoInicial = System.currentTimeMillis();
+						Mac mac = Mac.getInstance(macOption);
+						mac.init(symmetricKey);
+
+						byte[] bytesHMacEncrypt = mac.doFinal(montoEnByte);
+//						long tiempoFinal = System.currentTimeMillis();
+//						System.out.println("Tiempo de cifrado mac es: " + (tiempoFinal - tiempoInicial) + "ms");
+						String hashCifradoenString = printBase64Binary(bytesHMacEncrypt);
+
+						if(hMac.equals(hashCifradoenString))
+						{
+							clientWriter.println(Cliente.OK);
+							System.out.println("Consulta finalizada.");
+						}
+						else
+						{
+							System.out.println("Error. El hmac calculado no es igual al que se recibio");
+							clientWriter.println(Cliente.ERROR);
+						}
+					}
+					else
+					{
+						System.err.println(Cliente.ERROR);
 					}
 				}
 				else
 				{
-					System.err.println(Cliente.ERROR);
+					System.out.println("Error al enviar los algoritmos al servidor");
 				}
 			}
 			else
 			{
-				System.out.println("Error al enviar los algoritmos al servidor");
+				System.out.println("Error. El servidor no respondio de manera correcta.");
 			}
-		}
-		else
-		{
-			System.out.println("El servidor no respondio.");
-		}
 		}
 		catch (Exception e)
 		{
 			e.getMessage();
 		}
 	}
-	
+
 	public static String printBase64Binary(byte[] certificadoEnBytes) 
 	{
 		return DatatypeConverter.printBase64Binary(certificadoEnBytes);
 	}
-	
+
 	public static byte[] parserBase64Binary(String certificadoEnBytes) 
 	{
 		return DatatypeConverter.parseBase64Binary(certificadoEnBytes);
@@ -299,27 +247,29 @@ public class Protocolo {
 	{
 		try
 		{
-			
+
 			Provider pro = new BouncyCastleProvider();
 			Security.addProvider(pro);
-			//fecha inicial de expedicion
+			
+			//Fecha inicial de expedicion del certificado
 			long fechaActual = System.currentTimeMillis();
 			BigInteger sn = new BigInteger(Long.toString(fechaActual));
 			Date fechaDefinitiva = new Date(fechaActual);
 
 			X500Name name = new X500Name("CN=localhost");
-			
-			Date fechaFinal = new Date(System.currentTimeMillis());
-			//Firma
-			ContentSigner signer = new JcaContentSignerBuilder("SHA256WithRSA").build(kp.getPrivate());
-			//Lo genero con mi publica para que el server lo pueda leer
-			JcaX509v3CertificateBuilder constructorCertificador = new JcaX509v3CertificateBuilder(name, sn, fechaDefinitiva, fechaFinal, name, kp.getPublic());
-			
-			constructorCertificador.addExtension(new ASN1ObjectIdentifier("10.23.19"), true, new BasicConstraints(true));
-			
-			//certificado obtenido
-			X509Certificate certificado = new JcaX509CertificateConverter().setProvider(pro).getCertificate(constructorCertificador.build(signer));
 
+			Date fechaFinal = new Date(System.currentTimeMillis());
+			
+			//Firma que se hace sobre el certificado
+			ContentSigner signer = new JcaContentSignerBuilder("SHA256WithRSA").build(kp.getPrivate());
+			
+			//Lo genero con mi llave publica para que el server lo pueda leer y sea correcto
+			JcaX509v3CertificateBuilder constructorCertificador = new JcaX509v3CertificateBuilder(name, sn, fechaDefinitiva, fechaFinal, name, kp.getPublic());
+
+			constructorCertificador.addExtension(new ASN1ObjectIdentifier("10.23.19"), true, new BasicConstraints(true));
+
+			//Certificado finalizado
+			X509Certificate certificado = new JcaX509CertificateConverter().setProvider(pro).getCertificate(constructorCertificador.build(signer));
 
 			return certificado;
 
@@ -328,10 +278,12 @@ public class Protocolo {
 		{
 			ce.printStackTrace();
 			return null;
-		} catch (CertIOException e) 
+		} 
+		catch (CertIOException e) 
 		{
 			e.printStackTrace();
 		}
+		
 		return null;
 	}
 }
